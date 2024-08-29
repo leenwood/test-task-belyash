11feb323cfa0a29a0d5dccc0ecbb70e6405cbfe8
gperman <gperman@profitbase.ru>
2024-08-29 08:45:01 +0500

    hotfix

--- app/src/Repository/IngredientTypeRepository.php
+++ app/src/Repository/IngredientTypeRepository.php
@@ -15,29 +15,4 @@ class IngredientTypeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, IngredientType::class);
     }
-
-    //    /**
-    //     * @return IngredientType[] Returns an array of IngredientType objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('i')
-    //            ->andWhere('i.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('i.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?IngredientType
-    //    {
-    //        return $this->createQueryBuilder('i')
-    //            ->andWhere('i.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
